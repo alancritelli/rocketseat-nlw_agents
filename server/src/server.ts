83dd4e465ab29beace711478f63b59b43a328849
Alan Critelli <alancritelli@gmail.com>
2025-07-10 12:41:58 -0300

    chore: Add Postgres setup with Docker and database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import {
   type ZodTypeProvider
 } from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
+import { sql } from './db/connection.ts'
 import { env } from './env.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
